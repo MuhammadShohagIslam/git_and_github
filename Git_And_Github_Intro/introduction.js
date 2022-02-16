bd7ba87f8c6d9e8c1e97ba2973f912183825aca6
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-02-16 19:34:17 +0300

    Git and Github Intro done

---
+++ Git_And_Github_Intro/introduction.js
@@ -0,0 +1,32 @@
+/*
+    *** Git and Github Introduction ***
+
+        *** Git ***
+            => Through Git, we can work version control system
+            => Keep track of changes
+            => Collaboration on project and so on...
+
+        *** Other Version Control System ***
+            => GitLab
+            => Beanstalk
+            => PerForce
+            => Bitbucket
+            * Git is popular beacuse over 70% developers use Git!
+
+        *** Git Vs Gihub ***
+
+                Github                                                                       Git
+
+        1. Service                                                             1. Tool / Software
+        2. Host Git repository /      
+            Folder/Source code                                         2. Change control / version control
+
+        3. Provide GUI interface                                     3. Provide CLI(Command Line Interface)
+
+        4. Maintain on cloud web                                     4. Installed and maintain locally.
+
+        5. More than 56 million users                               5.Released: April 7, 2005
+
+        6. Founded 2008                                                   6. Python, c, C++, perl, (Linux Torvalds and Junio C Hamano )
+
+*/
