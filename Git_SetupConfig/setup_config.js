b0f447b24732164df0c6c13442f656175c12bcfa
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-02-16 21:24:47 +0300

    Git Setup And Config is done

---
+++ Git_SetupConfig/setup_config.js
@@ -0,0 +1,15 @@
+/*
+    *** Git Setup and Configaration ***
+    => (git --version), it's showed, which version git we have
+
+    *** Configaration ***
+        => globally configaration for user name (git config --global user.name "your desire name").
+        => locally configaration for user name (git config  user.email "your desire email").
+        => globally configaration for user email (git config --global user.email "your desire name").
+        => locally configaration for user email (git config  user.email "your desire email").
+            *** if use --global, it's means this user name use all and every git file for repository. Without --global, we can work with git specific git workplace.
+
+        => (git config --list) ---> we can see all of configaration list like username email
+        
+*/
+
