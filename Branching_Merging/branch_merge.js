46467d5e50f0e24c91dc737e6a7f82b07bb39754
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-02-19 21:51:41 +0300

    Branching theory is modified

--- Branching_Merging/branch_merge.js
+++ Branching_Merging/branch_merge.js
@@ -1,3 +1,4 @@
+
 /*
     *** Braching And Marging ***
       => Branch: Branch is a new and separate branch of the master repo.
@@ -7,8 +8,12 @@
     *** The way of working with branch through with git command line
          => git branch branch_name
          => git checkout branch_name (Through which, we can go master brach to new creating branch)
+                git checkout -b branch_name (both work is done, create branch and swith branch from main to branch_name)
          => git checkout master ( after finishing work, we can pass code to master brach)
          => git merge branch_name
+
+         *** for deleting branch, we can first come to main branch, after we can write following command
+                => git branch -d branch_name
          
      Credit by Anisul Islam(Software Engineer in Finland)
       
