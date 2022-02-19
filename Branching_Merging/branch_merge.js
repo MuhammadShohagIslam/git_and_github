47fc20dfefa642cdb6e2116c11cdf17f6203a965
Muhammad Shohag Islam <91692259+MuhammadShohagIslam@users.noreply.github.com>
2022-02-19 21:19:10 +0300

    Practice done with theory of merging and branching

--- Branching_Merging/branch_merge.js
+++ Branching_Merging/branch_merge.js
@@ -1,5 +1,15 @@
 /*
     *** Braching And Marging ***
-    
-
+      => Branch: Branch is a new and separate branch of the master repo.
+      => In a big project we separate the tasks / features and create branch so editing in the 
+         new branch does not affect the master branch.
+         
+    *** The way of working with branch through with git command line
+         => git branch branch_name
+         => git checkout branch_name (Through which, we can go master brach to new creating branch)
+         => git checkout master ( after finishing work, we can pass code to master brach)
+         => git merge branch_name
+         
+     Credit by Anisul Islam(Software Engineer in Finland)
+      
 */
