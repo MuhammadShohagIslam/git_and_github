82297ac8844a23059c9925eb19de81116dadf76f
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-02-20 20:08:38 +0300

    Added Theory of Merge Conflict

---
+++ Merge_Conflict/merge_conflict.js
@@ -0,0 +1,8 @@
+/*
+    *** Merge Conflict ***
+        => When we are going to work main/master and another branch, 
+        and at a same time, we are going to commit same branch same line, 
+        in that time we are facing merge confict, both branch when we want to merge with the main/master.
+
+    => When we face this problem, we should resolve this confict by deleteing same code, which we do not want. 
+*/
